6afea6b6b38e92d1ed5a7d3f842a58df954f059d
chaoswo <93654687+chaoswo@users.noreply.github.com>
2024-05-02 20:41:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,9 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
+    args = parser.parse_args()
+    
+    app.run(debug=True, port=args.port)
